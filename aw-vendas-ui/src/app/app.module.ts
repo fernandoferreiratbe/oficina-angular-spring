95a26eb17baf474fc408cc96174dbedddddf0f7e
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2018-10-23 09:17:04 -0300

    First commit
    - aw-vendas-api - Spring project
    - aw-vendas-ui  - Angular project

---
+++ aw-vendas-ui/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+import { TableModule } from 'primeng/table';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { PanelModule } from 'primeng/panel';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { VendasListagemComponent } from './vendas-listagem/vendas-listagem.component';
+import { VendaCadastroComponent } from './venda-cadastro/venda-cadastro.component';
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    VendasListagemComponent,
+    VendaCadastroComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+
+    CurrencyMaskModule,
+    TableModule,
+    DropdownModule,
+    InputTextModule,
+    PanelModule
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt'}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ aw-vendas-ui/src/app/vendas/vendas.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VendasService {
+
+  private api = 'http://localhost:8080'; // This is the address to our API.
+
+  constructor(private http: HttpClient) { }
+
+  listar(): Observable<any> {
+    return this.http.get<any>(`${this.api}/vendas`);
+  }
+
+  listarClientes(): Observable<any> {
+    return this.http.get<any>(`${this.api}/clientes`);
+  }
+
+  listarProdutos(): Observable<any> {
+    return this.http.get<any>(`${this.api}/produtos`);
+  }
+}
